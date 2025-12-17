7734e987bc6545818bea8edd42880f73975cae5a
Lgsus23 <jesusbernardops23@gmail.com>
2025-12-17 19:01:27 +0000

    estética hecha

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -3,26 +3,22 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+import '../../styles/index.css'
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container text-center justify-content-around d-flex mx-auto">
+			<div className="fondo d-flex text-center my-5 mx-auto">
+				<div className="digito me-3"><i className="fa-regular fa-clock"></i></div>
+				<div className="digito">1</div>
+				<div className="digito me-3">2</div>
+				<div className="digito">3</div>
+				<div className="digito me-3">4</div>
+				<div className="digito">5</div>
+				<div className="digito">6</div>
+			</div>
 		</div>
-	);
+	)
 };
 
 export default Home;
